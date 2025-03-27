df732b094ac3fae5dd6db9f957b29e2d7780aeea
Lello <Lello@11-4.wizard.justplayer.com>
2025-03-27 12:30:34 +0900

    Made a different function to call the BTC information, this function will have editable parameters also it can be generalized

--- test/call.js
+++ test/call.js
@@ -1,20 +1,20 @@
-//Simple call to coingecko API to know the price of the bitcoins USD
-fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
-.then(function(response){
-    if(response.ok){ //this property of response allows us to check if the Server responded with a 200 range HTTP status code
-        console.log(response.status)
-        return response.json();
-    }else{
-        throw new Error("Could not contact the Server");
-    }
-})
-.then(function(btcPrice){
-    console.log(btcPrice);
-})
-.catch(function(error){
-    console.log(error);
-})
+//Coded this way is better as I can generalize the API Call for different cryptos, also more control over parameters
+function getBTCInfo(){
+    return new Promise((resolve,reject) => {
+        let getMarketCap = false;
+        let precision = 0;
+        let currency = "usd";
+        let URL = "https://api.coingecko.com/api/v3/simple/price?" +
+            "ids=bitcoin" //This can be changed, the function itself can be made general so to get information from different tokens
+            + "&vs_currencies=" + currency
+            + "&include_market_cap=" + getMarketCap
+            + "&precision=" + precision;
 
-/*Note:
-Maybe is not a good choice to not use anonymous functions, it makes the code a little messy
-*/
+        fetch(URL)
+            .then((data) => {return data.json()})
+            .then((data) => console.log(data))
+            .catch((error) => console.log(error))
+    })
+}
+
+getBTCInfo();
