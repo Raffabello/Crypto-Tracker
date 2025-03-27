0bb6d28a237da7a0ff0c8469fc28b3ba76f95351
Raffabello <that.raffaele95bin@gmail.com>
2025-03-28 00:57:21 +0100

    Created a new function that gets all the tokens still experimental but this endpoint returns also market data

--- test/call.js
+++ test/call.js
@@ -17,4 +17,28 @@ function getBTCInfo(){
     })
 }
 
-getBTCInfo();
+function getTokens(){
+    return fetch("https://api.coingecko.com/api/v3/coins/list")
+        .then((data) => data.json())
+        .then((coins) => {return coins})
+        .catch((error) => {return error})
+}
+
+//Function to get tokens but with market data
+function getTokens2(){
+    let tokens = new Promise((resolve,reject) => {
+        let URL = "https://api.coingecko.com/api/v3/coins/markets?order=market_cap_desc&vs_currency=usd";
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if(xhttp.readyState == xhttp.DONE){
+                if(xhttp.status === 200){
+                    return resolve(xhttp.response);
+                }else{
+                    reject("Tokens could not be retrieved")
+                }
+            }
+        }
+        xhttp.open("GET", URL);
+        xhttp.send();
+    })
+}
