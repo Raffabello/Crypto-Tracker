4257ee77310d143f81a36f172440ddc8cbb2bc41
Raffabello <that.raffaele95bin@gmail.com>
2025-04-02 15:27:00 +0200

    Now tokens can be fetched, improved the fetch logic with a better error handling

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,28 @@
-alert("Test")
+function getTokensInfo(){
+    const URL = "https://api.coingecko.com/api/v3/coins/markets?order=market_cap_desc&vs_currency=usd&per_page=5";
+    return new Promise((resolve,reject) => {
+        fetch(URL)
+            .then(function(response){
+                if(!response.ok){
+                    reject("Network Error, could not retrieve Token Market Data");
+                }else{
+                    if(response.status === 200){
+                        return response.json();
+                    }else{
+                        reject("Server successfully answered, but the data was not retrievable\nThe status code received is " + response.status);
+                    }
+                }
+            })
+            .then(function(tokens){
+                resolve(tokens);
+            })
+    })
+}
+
+async function loadTokensMarketData(){
+    try{
+        let tokens = await getTokensInfo();
+    }catch(error){
+        console.log(error);
+    }
+}
