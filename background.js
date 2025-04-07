5bc8350923eaaadd3d5e9f9dd5eb9b4a528ae542
Raffabello <that.raffaele95bin@gmail.com>
2025-04-07 15:07:26 +0200

    Added background script to cache tokens prices

---
+++ background.js
@@ -0,0 +1,72 @@
+const TIME_BEFORE_CALL = 300000;
+const MAX_CACHE_SIZE = 6;
+
+function getTokensPrices(){
+    const URL = "https://api.coingecko.com/api/v3/coins/markets?order=market_cap_desc&vs_currency=usd&per_page=5";
+    return new Promise((resolve,reject) => {
+        fetch(URL)
+            .then(function(response){
+                if(!response.ok){
+                    reject("Network Error, could not retrieve Token Market Data");
+                }else{
+                    if(response.status === 200){
+                        return response.json();
+                    }else{
+                        reject("Connected successfully , but the data was not retrievable\nThe status code received is " + response.status);
+                    }
+                }
+            })
+            .then(function(tokens){
+                let currentTime = new Date();
+                let currentHour = currentTime.getHours().toString().padStart(2,"0");
+                let currentMinute = currentTime.getMinutes().toString().padStart(2,"0");
+                let ampm = currentHour >= 12 ? "PM" : "AM";
+                currentTime = currentHour + ":" + currentMinute + ampm;
+
+                chrome.storage.local.get("Crypto-tracker-cache", (items) => {
+                    if(items.hasOwnProperty("Crypto-tracker-cache")){
+                        //Values are recorded
+                        let storedArray = items["Crypto-tracker-cache"];
+                        let currentPairs = storedArray[0].pairs.length; //Ref is the BTC
+                        if(currentPairs > MAX_CACHE_SIZE - 1){
+                            chrome.storage.local.clear(() => {
+                                let cachedArray = tokens.map(function(token){
+                                    return {
+                                        name:token.name,
+                                        pairs:[{x:currentTime, y:token.current_price}]
+                                    }
+                                })
+                                chrome.storage.local.set({"Crypto-tracker-cache":cachedArray})
+                            })
+                        }else{
+                            storedArray.forEach(function(storedArrayItem){
+                                tokens.forEach(function(tokenArrayItem){
+                                    if(storedArrayItem.name === tokenArrayItem.name){
+                                        storedArrayItem.pairs.push({x:currentTime,y:tokenArrayItem.current_price});
+                                    }
+                                })
+                            })
+                            chrome.storage.local.set({"Crypto-tracker-cache":storedArray});
+                        }
+                    }else{
+                        let cachedArray = tokens.map(function(token){
+                            return {
+                                name:token.name,
+                                pairs:[{x:currentTime, y:token.current_price}]
+                            }
+                        })
+                        chrome.storage.local.set({"Crypto-tracker-cache":cachedArray})
+                    }
+                })
+                return resolve(tokens)
+            })
+            .catch(function(error){
+                reject(error)
+            })
+    })
+}
+
+setInterval(() =>{
+    getTokensPrices();
+}, TIME_BEFORE_CALL)
+
