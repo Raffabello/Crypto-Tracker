12492db18e96b25e5f7806f29aae9f6c0e7df145
Lello <Lello@18-4.wizard.justplayer.com>
2025-04-09 13:26:48 +0900

    Added two function to display the correct selected currency

--- popup/script/currency.js
+++ popup/script/currency.js
@@ -1,1 +1,16 @@
-console.log("Hello")
+function getUSDFormat(price){
+    let priceUSD = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    }).format(price);
+    return priceUSD;
+}
+
+//Test
+function getEURFormat(price){
+    let priceEUR = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'EUR',
+    }).format(price);
+    return priceEUR;
+}
