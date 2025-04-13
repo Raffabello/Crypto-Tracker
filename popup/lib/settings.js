fe8515ef9ce93aaf6665c1ebe12263e5ed8542a9
Raffabello <that.raffaele95bin@gmail.com>
2025-04-13 05:18:22 +0200

    Correct commit, before changed call order, first settings are loaded, then call is made Now, added loadSetting function to get and load the settings in the popup

--- popup/lib/settings.js
+++ popup/lib/settings.js
@@ -1,6 +1,43 @@
-//Other settings
+let applySettingButton = document.getElementById("settings-confirm-button");
+let currencySetting = document.getElementById("settings-choose-currency-select");
 let settingsButton = document.getElementById("settings-button");
 let settingsWindow = document.getElementById("settings");
+let settingsMap = {};
+
+function getSettings(){
+    return new Promise((resolve) => {
+        chrome.storage.local.get("Crypto-currency-tracker-settings", (cachedSettings) =>{
+            resolve(cachedSettings);
+        })
+    })
+}
+
+async function loadSettings(){
+    try {
+        let settings = await getSettings();
+        if(Object.keys(settings).length === 0){
+            settingsMap = {
+                currency:"usd"
+            }
+        }else{
+            //The setting map exists
+            settingsMap = {
+                currency:settings["Crypto-currency-tracker-settings"]["currency"]
+            }
+            Array.from(currencySetting).forEach((node) => {
+                if(node.innerText.toLowerCase() === settings["Crypto-currency-tracker-settings"]["currency"]){
+                    console.log("here")
+                    node.selected = true;
+                }
+            })
+        }
+    }
+    catch(error){
+        return error; //小几率
+    }
+}
+
+//Other settings
 
 settingsButton.addEventListener("click", function(){
     if(settingsWindow.classList.contains("settings-hidden")){
@@ -12,25 +49,21 @@ settingsButton.addEventListener("click", function(){
     }
 })
 
-let applySettingButton = document.getElementById("settings-confirm-button");
-
-let settingsMap = {
-    currency:"usd"
-}
-
-//Options
-let currencySetting = document.getElementById("settings-choose-currency-select");
 currencySetting.addEventListener("change", function(event){
     settingsMap["currency"] = event.target.value;
 })
 
 applySettingButton.addEventListener("click", function(){
-    const cachedSettingsName = "Crypto-currency-tracker-settings";
     //Apply to the session
     for(key of Object.keys(settingsMap)){
         if(key === "currency"){
             //Apply currency settings
-            loadTokensMarketData(displayTokensMarketData)
+            loadTokensMarketData(displayTokensMarketData);
         }
     }
+    chrome.storage.local.set({"Crypto-currency-tracker-settings":settingsMap});
+
+    //Close window
+    settingsWindow.classList.remove("settings-visible");
+    settingsWindow.classList.add("settings-hidden");
 })
